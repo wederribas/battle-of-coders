f75c363d4e3e1c688e39daba9853b09f293f9919
Weder Ribas <wederribas@gmail.com>
2017-09-23 15:36:25 -0300

    Create new App component to render child components

--- app/index.js
+++ app/index.js
@@ -2,34 +2,9 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 require('./index.css');
-
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <img
-          src={this.props.img}
-          alt='Avatar'
-          style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>Username: {this.props.username}</h3>
-      </div>
-    )
-  }
-}
-
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-}
+var App = require('./components/App');
 
 ReactDOM.render(
-  <Badge
-    name='Weder Ribas'
-    username='wederribas'
-    img={'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/2/005/070/1b9/2b6b46c.jpg'}
-  />,
-  document.getElementById('root')
+  <App/>,
+  document.getElementById('app')
 );
