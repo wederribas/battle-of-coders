dab5e78ab139310b47b2bd60d5fb2036ac14e7a5
Weder Ribas <wederribas@gmail.com>
2017-09-26 20:50:21 -0300

    Add new Loading component to Popular and Results components

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 function SelectedLanguage(props) {
 	var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -90,7 +91,7 @@ class Popular extends React.Component {
 					onSelect={this.updateLanguage}
 				/>
 				{!this.state.repos
-					? <p>LOADING</p>
+					? <Loading />
 					: <RepoGrid repos={this.state.repos}/> }
 			</div>
 		)
