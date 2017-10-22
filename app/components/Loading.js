493151b06f761d879d823f289bf2fc0f74e81150
Weder Ribas <wederribas@gmail.com>
2017-10-22 12:04:49 -0200

    Add babel class properties dependency and change components with the new properties

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -9,11 +9,17 @@ const styles = {
 };
 
 class Loading extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: props.text
-    }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+  
+  }
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300
+  }  
+  state = {
+    text: this.props.text
   }
   componentDidMount() {
     const { text, speed } = this.props;
@@ -36,16 +42,5 @@ class Loading extends React.Component {
     )
   }
 }
-
-Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
-
-};
-
-Loading.defaultProps = {
-  text: 'Loading',
-  speed: 300
-};
 
 export default Loading;
