1c81a04cb0ea8d36b66f02267947c16066c644d5
Weder Ribas <wederribas@gmail.com>
2017-10-22 13:40:11 -0200

    Change promises to the async/await format

--- app/components/Results.js
+++ app/components/Results.js
@@ -50,26 +50,26 @@ class Results extends React.Component {
     error: null,
     loading: true
   }
-  componentDidMount () {
+  async componentDidMount () {
     const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
 
-    battle([
+    const players = await battle([
       playerOneName,
       playerTwoName
-    ]).then((results) => {
-      if (results === null) {
-        return this.setState(() => ({
-          error: 'An error happened. Check if both users exist on GitHub',
-          loading: false
-        }));
-      }
-      this.setState(() => ({
-        error: null,
-        winner: results[0],
-        loser: results[1],
-        loading: false,
+    ]);
+
+    if (players === null) {
+      return this.setState(() => ({
+        error: 'An error happened. Check if both users exist on GitHub',
+        loading: false
       }));
-    });
+    }
+    this.setState(() => ({
+      error: null,
+      winner: players[0],
+      loser: players[1],
+      loading: false,
+    }));
   }
   render() {
     const { error, winner, loser, loading } = this.state;
